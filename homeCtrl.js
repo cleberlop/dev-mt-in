c3b867ef88791cd4caae2f3cb5ba03fdf6c3f6a7
Cleber Albuquerque <cleber.lop@gmail.com>
2015-12-17 16:49:10 -0700

    finished practice

---
+++ homeCtrl.js
@@ -0,0 +1,78 @@
+angular.module('devMtIn').controller('homeCtrl', function($scope, profileService, friendService){
+
+  $scope.checkForProfile = function() {
+  var profileId = JSON.parse(localStorage.getItem('profileId'));
+
+  if (profileId) {
+    profileService.checkForProfile(profileId.profileId)
+    .then(function(profile) {
+      $scope.myProfile = profile.data;
+      friendService.findFriendsFriends(profile.data);
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
+  }
+};
+
+  $scope.checkForProfile();
+
+  $scope.sortOptions = [{
+    display: 'Ascending',
+    value: false
+  },
+  {
+    display: "Descending",
+    value: true,
+  }];
+
+profileService.serviceTest();
+
+$scope.editing = false;
+
+$scope.saveProfile = function(profile) {
+  profileService.saveProfile(profile);
+  $scope.editing = false;
+
+};
+$scope.deleteProfile = function() {
+  profileService.deleteProfile()
+  .then(function(deleteProfile){
+    localStorage.removeItem('profileId');
+    $scope.myProfile = {};
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
+
+};
+
+$scope.findFriends = function(query) {
+  friendService.findFriends($scope.myProfile._id, query)
+  .then(function(response){
+    $scope.potentialFriends = response.data;
+    console.log(response.data);
+
+  });
+};
+
+$scope.addFriend = function(friendId) {
+  friendService.addFriend($scope.myProfile._id, friendId).then(function() {
+    $scope.checkForProfile();
+  });
+};
+
+$scope.removeFriend = function(friendId) {
+  friendService.removeFriend($scope.myProfile._id, friendId).then($scope.checkForProfile());
+
+};
+
+
+
+
+
+
+
+
+
+});
